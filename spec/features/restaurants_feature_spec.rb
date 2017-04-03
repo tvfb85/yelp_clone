d229b3da3763c34d7176711a3244d78669540533
Vicky Roberts <vicki_uk@hotmail.com>
2017-04-03 10:42:44 +0100

    Initial commit: Rails setup, db and MVC for Restaurant added

---
+++ spec/features/restaurants_feature_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'restaurants' do
+
+  context 'no restaurants have been added' do
+
+    scenario 'should display a prompt to add a restaurant' do
+      visit '/restaurants'
+      expect(page).to have_content 'No restaurants yet'
+      expect(page).to have_link 'Add a restaurant'
+    end
+
+  end
+
+  context 'restaurants have been added' do
+
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('KFC')
+      expect(page).not_to have_content('No restaurants yet')
+    end
+
+  end
+
+end
